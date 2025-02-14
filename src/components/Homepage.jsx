1ee572dd7558dde300915bf0410187ec2327966c
Alessandro-Marabese <alessandro.marabese93@gmail.com>
2025-02-14 16:50:25 +0100

    second commit

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,5 +1,6 @@
 import { Card, CardImg, Col, Container, Form, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import NavBar from "./NavBar";
 
 const Homepage = (props) => {
   
@@ -8,7 +9,7 @@ const Homepage = (props) => {
         <Row className="w-75 mx-auto pt-5 ">
           <Col className="px-0 mt-4">
             <Card className="border-0 ">
-              <CardImg src="https://static.vecteezy.com/system/resources/thumbnails/010/989/526/small_2x/rainy-weather-3d-rendering-isolated-on-transparent-background-ui-ux-icon-design-web-and-app-trend-png.png" className="mt-5" />
+              <CardImg src="https://static.vecteezy.com/system/resources/thumbnails/010/989/526/small_2x/rainy-weather-3d-rendering-isolated-on-transparent-background-ui-ux-icon-design-web-and-app-trend-png.png" className="mt-2" />
             </Card>
           </Col>
         </Row>
@@ -20,6 +21,7 @@ const Homepage = (props) => {
           </Form.Group>
           <Link to="/details" className="btn btn-success mt-5 mb-5">Search</Link>          
         </Form>
+        <NavBar />
       </Container>
     )
   }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,32 @@
-import { Container, Nav, Navbar } from "react-bootstrap"
+import { Container, Nav } from "react-bootstrap"
 import { Link } from "react-router-dom"
 
 const NavBar = () => {
-    return
+    return(
+        <Container id="navbar" className="text-white mt-4 rounded d-flex justify-content-around">
+                <Link to="/" className="nav-link d-inline-block pt-2 pb-5">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-house" viewBox="0 0 16 16">
+                            <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
+                        </svg>
+                </Link>
+                <Nav.Link href="#" className="d-inline-block pt-2 pb-5">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                        </svg>
+            </Nav.Link>            
+            <Nav.Link href="#" className="d-inline-block pt-2 pb-5">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-geo-alt" viewBox="0 0 16 16">
+                            <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"/>
+                            <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                        </svg>
+            </Nav.Link>            
+            <Nav.Link href="#" className="d-inline-block pt-2 pb-5">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                            <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+                        </svg>
+            </Nav.Link>
+        </Container>
+    )
 }
 export default NavBar
